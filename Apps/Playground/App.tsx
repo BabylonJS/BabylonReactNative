ea33902c2931809a9d8948618345d1463940ccdf
Ryan Tremblay <ryan.tremblay@microsoft.com>
2020-04-07 17:25:38 -0700

    Adding initial implementation for Android integration (#3)
    This adds the initial integration of Babylon Native into React Native for Android specifically. The changes are described in readme.md.
    
    This is the third of three initial commits to create a reasonable git history from the work that has been done so far. The three commits will be:
    
    - A "blank" Playground React Native app ✔
    - A "blank" react-native-babylon React Native module ✔
    - A fleshed out initial (Android) implementation of react-native-babylon module and Playground app to test it

--- Apps/Playground/App.tsx
+++ Apps/Playground/App.tsx
@@ -1,118 +1,104 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
  * Generated with the TypeScript template
  * https://github.com/react-native-community/react-native-template-typescript
  *
  * @format
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, { useState, FunctionComponent, useEffect } from 'react';
+import { SafeAreaView, StatusBar, Button, View, Text, ViewProps } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { EngineView, useEngine } from 'react-native-babylon';
+import { Scene, Vector3, Mesh, ArcRotateCamera, Engine, Camera, PBRMetallicRoughnessMaterial, Color3 } from '@babylonjs/core';
+import Slider from '@react-native-community/slider';
 
-declare var global: {HermesInternal: null | {}};
+const EngineScreen: FunctionComponent<ViewProps> = (props: ViewProps) => {
+  const defaultScale = 1;
 
-const App = () => {
+  const [toggleView, setToggleView] = useState(false);
+  const [camera, setCamera] = useState<Camera>();
+  const [box, setBox] = useState<Mesh>();
+  const [scale, setScale] = useState<number>(defaultScale);
+  const [fps, setFps] = useState<number>(0);
+
+  useEngine((engine: Engine) => {
+    const scene = new Scene(engine);
+    scene.createDefaultCamera(true);
+    if (scene.activeCamera != null) {
+      (scene.activeCamera as ArcRotateCamera).beta -= Math.PI / 8;
+      setCamera(scene.activeCamera);
+    }
+    scene.createDefaultLight(true);
+
+    const box = Mesh.CreateBox("box", 0.3, scene);
+    setBox(box);
+    const mat = new PBRMetallicRoughnessMaterial("mat", scene);
+    mat.metallic = 1;
+    mat.roughness = 0.5;
+    mat.baseColor = Color3.Red();
+    box.material = mat;
+
+    scene.beforeRender = function () {
+      scene.meshes[0].rotate(Vector3.Up(), 0.005 * scene.getAnimationRatio());
+    };
+
+    const timerHandle = setInterval(() => {
+      setFps(engine.getFps());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerHandle);
+    };
+  });
+
+  useEffect(() => {
+    if (box) {
+      box.scaling = new Vector3(scale, scale, scale);
+    }
+  }, [box, scale]);
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+      <View style={props.style}>
+        <Button title="Toggle EngineView" onPress={() => { setToggleView(!toggleView) }} />
+        { !toggleView &&
+          <View style={{flex: 1}}>
+            <EngineView style={props.style} camera={camera} />
+            <Slider style={{position: 'absolute', minHeight: 50, margin: 10, left: 0, right: 0, bottom: 0}} minimumValue={0.2} maximumValue={2} value={defaultScale} onValueChange={setScale} />
+            <Text style={{color: 'yellow', position: 'absolute', margin: 10, right: 0, top: 0}}>FPS: {Math.round(fps)}</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
+        }
+        { toggleView &&
+          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{fontSize: 24}}>EngineView has been removed.</Text>
+            <Text style={{fontSize: 12}}>Render loop stopped, but engine is still alive.</Text>
+          </View>
+        }
+      </View>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const App = () => {
+  const [toggleScreen, setToggleScreen] = useState(false);
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{flex: 1}}>
+        { !toggleScreen &&
+          <EngineScreen style={{flex: 1}} />
+        }
+        { toggleScreen &&
+          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{fontSize: 24}}>EngineScreen has been removed.</Text>
+            <Text style={{fontSize: 12}}>Engine has been disposed, and will be recreated.</Text>
+          </View>
+        }
+        <Button title="Toggle EngineScreen" onPress={() => { setToggleScreen(!toggleScreen) }} />
+      </SafeAreaView>
+    </>
+  );
+};
 
 export default App;
---
+++ README.md
@@ -0,0 +1,140 @@
+# Babylon React Native
+
+This project provides Babylon Native integration into React Native.
+
+## Current Status
+
+Babylon React Native is in the early phase of its development, and has the following limitations:
+
+1. Android support only - support for both iOS and Windows is planned.
+1. JavaScriptCore only - support for Hermes is planned, and support for other JavaScript engines used by React Native is uncertain.
+
+## Consuming
+
+This quick overview will help you understand the constructs provided by Babylon React Native and how to use them in a React Native application. See the Playground app's [App.tsx](Apps/Playground/App.tsx) for example usage.
+
+### `useEngine`
+
+`useEngine` is a **custom React hook** that manages the lifecycle of a Babylon engine instance in the context of an owning React component. A callback is passed to `useEngine` that receives an engine instance which is used to create and configure scenes. For example:
+
+```tsx
+import { useEngine } from 'react-native-babylon';
+import { Engine, Scene } from '@babylonjs/core';
+
+const MyComponent: FunctionComponent<MyComponentProps> = (props: MyComponentProps) => {
+    useEngine((engine: Engine) => {
+        const scene = new Scene(engine);
+        // Setup the scene!
+    });
+
+    return (
+        <>
+        </>
+    );
+}
+```
+
+### `EngineView`
+
+`EngineView` is a **custom React Native view** that presents a `camera` from a Babylon `scene`. A `camera` therefore is assigned to the `EngineView`. For example:
+
+```tsx
+import { useEngine, EngineView } from 'react-native-babylon';
+import { Engine, Scene, Camera } from '@babylonjs/core';
+
+const MyComponent: FunctionComponent<MyComponentProps> = (props: MyComponentProps) => {
+    const [camera, setCamera] = useState<Camera>();
+
+    useEngine((engine: Engine) => {
+        const scene = new Scene(engine);
+        scene.createDefaultCamera(true);
+        if (scene.activeCamera) {
+            setCamera(scene.activeCamera);
+        }
+        // Setup the scene!
+    });
+
+    return (
+        <>
+            <EngineView style={{flex: 1}} camera={camera} />
+        </>
+    );
+}
+```
+
+Note: Currently only one `EngineView` can be active at any given time. Multi-view will be supported in a future release.
+
+## Contributing
+
+This quick overview will help you get started developing in the Babylon React Native repository. We support development on Windows and macOS, but assume the use of [PowerShell](https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell) in the instructions below.
+
+### **Preparing a new Repo**
+
+**Required Tools:** [git](https://git-scm.com/), [Yarn](https://classic.yarnpkg.com/en/docs/install)
+
+Step 1 for all development environments and targets is to clone the repo. Use a git-enabled terminal to follow the steps below.
+
+```
+git clone https://github.com/BabylonJS/BabylonReactNative
+```
+
+Babylon React Native makes extensive use of submodules to supply its dependencies, so it's also necessary to set up the submodules.
+
+```
+cd BabylonReactNative
+git submodule update --init --recursive
+```
+
+The Playground sample/test app is a standard React Native app, and as such also makes extensive use of NPM packages to supply its dependencies, so it's also necessary to install these packages.
+
+```
+cd Apps/Playground
+yarn install
+```
+
+### **Configuring a Mac Dev Environment**
+
+**Required Tools:** [Android Studio](https://developer.android.com/studio/), [CMake](https://cmake.org/)
+
+- The `PATH` environment variable must include the path to adb (typically ~/Library/Android/sdk/platform-tools/).
+- The `ANDROID_HOME` environment variable must be defined (typically ~/Library/Android/sdk).
+- The `ANDROID_SDK_ROOT` environment variable must be defined (typically ~/Library/Android/sdk).
+- The `ANDROID_AVD_HOME` environment variable must be defined if you plan to use Android emulators (typically ~/.android/avd).
+
+You can typically configure your environment by editing `~/.zshrc` and adding the following:
+
+```
+export PATH=$PATH:~/Library/Android/sdk/platform-tools/
+export ANDROID_HOME=~/Library/Android/sdk
+export ANDROID_SDK_ROOT=~/Library/Android/sdk
+export ANDROID_AVD_HOME=~/.android/avd
+```
+
+### **Configuring a Windows Dev Environment**
+
+**Required Tools:** [Android Studio](https://developer.android.com/studio/), [CMake](https://cmake.org/)
+
+- The `PATH` environment variable must include the path to adb (typically %LOCALAPPDATA%/Android/sdk/platform-tools/).
+- The `ANDROID_HOME` environment variable must be defined (typically %LOCALAPPDATA%/Android/sdk).
+
+### **Building and Running the Playground App**
+
+On either Mac or Windows, NPX is used to build and run the Playground sample/test app from the command line. Open a command prompt at the root of the BabylonReactNative repo if you don't have one already open.
+
+```
+cd Apps/Playground
+npx react-native run-android
+```
+
+After having run the above command, you can also open `Apps/Playground/android` in Android Studio and run the app from there.
+
+### **Building the NPM Package**
+
+If you want to test using a local build of the NPM package with your own React Native app, you can do so with the `npm pack` command on either Mac or Windows.
+
+```
+cd Apps/Playground/node_modules/react-native-babylon
+npm pack
+```
+
+This will produce a zipped local NPM package that can be installed into a React Native application for testing purposes.
