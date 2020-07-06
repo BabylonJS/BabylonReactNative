6787cc8ec5c76179e9bc3e589cb986ba78a19ec3
Ryan Tremblay <ryan.tremblay@microsoft.com>
2020-07-06 12:46:07 -0700

    Remove runCommand and use src/pipe/dest
    Remove Build.sh
    
    Update Readme regarding creating packages

--- Package/gulpfile.js
+++ Package/gulpfile.js
@@ -1,29 +1,13 @@
 const util = require('util');
 const fs = require('fs');
-const exec = require('child_process').exec;
 const spawn = require('child_process').spawn;
-const execAsync = util.promisify(exec);
 const readdirAsync = util.promisify(fs.readdir);
 const log = require('fancy-log');
 const del = require('del');
 const gulp = require('gulp');
 const parseArgsStringToArgv = require('string-argv').parseArgsStringToArgv;
 
-async function runCommand(command, options = null, logCommand = true) {
-  if (logCommand) {
-    log(command);
-  }
-
-  const { stdout, stderr } = await execAsync(command, options);
-  if (stderr) {
-    log.error(stderr);
-  }
-  if (stdout) {
-    log(stdout);
-  }
-}
-
-async function runProcess(command, options = {}, logCommand = true) {
+async function exec(command, options = {}, logCommand = true) {
   if (logCommand) {
     log(command);
   }
@@ -54,61 +38,71 @@ const clean = async () => {
 };
 
 const makeXCodeProj = async () => {
-  await runCommand('mkdir -p iOS/Build');
-  await runProcess('cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../../Apps/Playground/node_modules/@babylonjs/react-native/submodules/BabylonNative/Dependencies/ios-cmake/ios.toolchain.cmake -DPLATFORM=OS64COMBINED -DENABLE_ARC=0 -DDEPLOYMENT_TARGET=12 -DENABLE_GLSLANG_BINARIES=OFF -DSPIRV_CROSS_CLI=OFF ..', {cwd: 'iOS/Build'});
+  await exec('mkdir -p iOS/Build');
+  await exec('cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../../Apps/Playground/node_modules/@babylonjs/react-native/submodules/BabylonNative/Dependencies/ios-cmake/ios.toolchain.cmake -DPLATFORM=OS64COMBINED -DENABLE_ARC=0 -DDEPLOYMENT_TARGET=12 -DENABLE_GLSLANG_BINARIES=OFF -DSPIRV_CROSS_CLI=OFF ..', {cwd: 'iOS/Build'});
 };
 
 const buildIphoneOS = async () => {
-  await runProcess('xcodebuild -sdk iphoneos -configuration Release -project ReactNativeBabylon.xcodeproj -scheme BabylonNative build CODE_SIGNING_ALLOWED=NO', {cwd: 'iOS/Build'});
+  await exec('xcodebuild -sdk iphoneos -configuration Release -project ReactNativeBabylon.xcodeproj -scheme BabylonNative build CODE_SIGNING_ALLOWED=NO', {cwd: 'iOS/Build'});
 };
 
 const buildIphoneSimulator = async () => {
-  await runProcess('xcodebuild -sdk iphonesimulator -configuration Release -project ReactNativeBabylon.xcodeproj -scheme BabylonNative build CODE_SIGNING_ALLOWED=NO', {cwd: 'iOS/Build'});
+  await exec('xcodebuild -sdk iphonesimulator -configuration Release -project ReactNativeBabylon.xcodeproj -scheme BabylonNative build CODE_SIGNING_ALLOWED=NO', {cwd: 'iOS/Build'});
 };
 
 const buildIOS = gulp.series(makeXCodeProj, buildIphoneOS, buildIphoneSimulator);
 
 const buildAndroid = async () => {
-  await runProcess('./gradlew babylonjs_react-native:assembleRelease', {cwd: '../Apps/Playground/android'});
+  await exec('./gradlew babylonjs_react-native:assembleRelease', {cwd: '../Apps/Playground/android'});
 };
 
 const copyCommonFiles = async () => {
-  await runCommand('mkdir -p Assembled');
-  await runCommand('cp ../Apps/Playground/node_modules/@babylonjs/react-native/package.json Assembled');
-  await runCommand('cp ../Apps/Playground/node_modules/@babylonjs/react-native/README.md Assembled');
-  await runCommand('cp ../Apps/Playground/node_modules/@babylonjs/react-native/*.ts* Assembled');
-  await runCommand('cp react-native-babylon.podspec Assembled');
+  return  gulp.src('../Apps/Playground/node_modules/@babylonjs/react-native/package.json')
+    .pipe(gulp.src('../Apps/Playground/node_modules/@babylonjs/react-native/README.md'))
+    .pipe(gulp.src('../Apps/Playground/node_modules/@babylonjs/react-native/*.ts*'))
+    .pipe(gulp.src('react-native-babylon.podspec'))
+    .pipe(gulp.dest('Assembled'));
 };
 
 const copyIOSFiles = async () => {
-  await runCommand('mkdir -p Assembled/ios');
-  await runCommand('cp ../Apps/Playground/node_modules/@babylonjs/react-native/ios/*.h Assembled/ios');
-  await runCommand('cp ../Apps/Playground/node_modules/@babylonjs/react-native/ios/*.mm Assembled/ios');
-  // This xcodeproj is garbage that we don't need in the package, but `pod install` ignores the package if it doesn't contain at least one xcodeproj. 🤷🏼‍♂️
-  await runCommand('cp -R iOS/Build/ReactNativeBabylon.xcodeproj Assembled/ios');
+  return  gulp.src('../Apps/Playground/node_modules/@babylonjs/react-native/ios/*.h')
+    .pipe(gulp.src('../Apps/Playground/node_modules/@babylonjs/react-native/ios/*.mm'))
+    // This xcodeproj is garbage that we don't need in the package, but `pod install` ignores the package if it doesn't contain at least one xcodeproj. 🤷🏼‍♂️
+    .pipe(gulp.src('iOS/Build/ReactNativeBabylon.xcodeproj**/**/*'))
+    .pipe(gulp.dest('Assembled/ios'));
 };
 
 const createIOSUniversalLibs = async () => {
-  await runCommand('mkdir -p Assembled/ios/libs');
+  await exec('mkdir -p Assembled/ios/libs');
   const libs = await readdirAsync('iOS/Build/Release-iphoneos');
-  await Promise.all(libs.map(lib => runCommand(`lipo -create iOS/Build/Release-iphoneos/${lib} iOS/Build/Release-iphonesimulator/${lib} -output Assembled/ios/libs/${lib}`)));
+  await Promise.all(libs.map(lib => exec(`lipo -create iOS/Build/Release-iphoneos/${lib} iOS/Build/Release-iphonesimulator/${lib} -output Assembled/ios/libs/${lib}`)));
 };
 
 const copyAndroidFiles = async () => {
-  await runCommand('mkdir -p Assembled/android');
-  await runCommand('cp Android/build.gradle Assembled/android');
+  await new Promise(resolve => {
+          gulp.src('Android/build.gradle')
+    .pipe(gulp.src('../Apps/Playground/node_modules/@babylonjs/react-native/android/src**/main/AndroidManifest.xml'))
+    .pipe(gulp.src('../Apps/Playground/node_modules/@babylonjs/react-native/android/src**/main/java/**/*'))
+    .pipe(gulp.dest('Assembled/android'))
+    .on('end', resolve);
+  });
 
-  await runCommand('mkdir -p Assembled/android/src/main');
-  await runCommand('cp ../Apps/Playground/node_modules/@babylonjs/react-native/android/src/main/AndroidManifest.xml Assembled/android/src/main');
+  await new Promise(resolve => {
+          gulp.src('../Apps/Playground/node_modules/@babylonjs/react-native/android/build/intermediates/library_and_local_jars_jni/release/jni/**/*')
+    .pipe(gulp.dest('Assembled/android/src/main/jniLibs/'))
+    .on('end', resolve);
+  });
+};
 
-  await runCommand('mkdir -p Assembled/android/src/main/java');
-  await runCommand('rsync -a ../Apps/Playground/node_modules/@babylonjs/react-native/android/src/main/java/ Assembled/android/src/main/java/');
-  await runCommand('rsync -a ../Apps/Playground/node_modules/@babylonjs/react-native/android/build/intermediates/library_and_local_jars_jni/release/jni/ Assembled/android/src/main/jniLibs/');
+const createPackage = async () => {
+  await exec('npm pack', {cwd: 'Assembled'});
 };
 
 const copyFiles = gulp.parallel(copyCommonFiles, copyIOSFiles, copyAndroidFiles);
 
 const build = gulp.series(buildIOS, buildAndroid, createIOSUniversalLibs, copyFiles);
+const rebuild = gulp.series(clean, build);
+const pack = gulp.series(rebuild, createPackage);
 
 exports.buildIOS = buildIOS;
 exports.buildAndroid = buildAndroid;
@@ -117,6 +111,7 @@ exports.copyFiles = copyFiles;
 
 exports.clean = clean;
 exports.build = build;
-exports.rebuild = gulp.series(clean, build);
+exports.rebuild = rebuild;
+exports.pack = pack;
 
 exports.default = build;
