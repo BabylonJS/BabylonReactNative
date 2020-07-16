145e837798b41ebe94f86cae41544b2805721743
Ryan Tremblay <ryan.tremblay@microsoft.com>
2020-07-15 19:27:05 -0700

    Upgrade React Native (Playground & PackageTest) to 0.63.0 (#48)
    This change upgrades the Playground and PackageTest apps to React Native 0.63.0. I decided to hold off on updating the actual @babylonjs/react-native dependencies on React Native for now since we are currently relying changes local to this repo that have not made it into a React Native release yet (e.g. JSCRuntime.cpp). Most of these changes are "boiler plate," in that I just recreated the apps from scratch and pulled forward the few customizations we'd previously made. Some notable parts of this change:
    
    - I updated the root .gitignore as some additional cleanup in this set of changes (I believe all the additions I made to .gitignore are generated files).
    - I noticed that bitcode was still not being enabled on our libs (I had tested this before, so I'm not sure why it's not working now). I put a comment in the CMakeLists.txt and for now updated the readme and the build script to enable bitcode in the cmake command line.
    - I updated the gulp build to validate the set of files that are being published for the package. This is to help prevent us from accidentally publishing a broken package.

--- Package/gulpfile.js
+++ Package/gulpfile.js
@@ -28,7 +28,7 @@ const clean = async () => {
 
 const makeXCodeProj = async () => {
   shelljs.mkdir('-p', 'iOS/Build');
-  exec('cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../../Apps/Playground/node_modules/@babylonjs/react-native/submodules/BabylonNative/Dependencies/ios-cmake/ios.toolchain.cmake -DPLATFORM=OS64COMBINED -DENABLE_ARC=0 -DDEPLOYMENT_TARGET=12 -DENABLE_GLSLANG_BINARIES=OFF -DSPIRV_CROSS_CLI=OFF ..', 'iOS/Build');
+  exec('cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../../Apps/Playground/node_modules/@babylonjs/react-native/submodules/BabylonNative/Dependencies/ios-cmake/ios.toolchain.cmake -DPLATFORM=OS64COMBINED -DENABLE_ARC=0 -DENABLE_BITCODE=1 -DDEPLOYMENT_TARGET=12 -DENABLE_GLSLANG_BINARIES=OFF -DSPIRV_CROSS_CLI=OFF ..', 'iOS/Build');
 };
 
 const buildIphoneOS = async () => {
@@ -77,11 +77,88 @@ const copyAndroidFiles = async () => {
   });
 
   await new Promise(resolve => {
-          gulp.src('../Apps/Playground/node_modules/@babylonjs/react-native/android/build/intermediates/library_and_local_jars_jni/release/jni/**/*')
+          gulp.src('../Apps/Playground/node_modules/@babylonjs/react-native/android/build/intermediates/library_and_local_jars_jni/release/**/*')
     .pipe(gulp.dest('Assembled/android/src/main/jniLibs/'))
     .on('end', resolve);
   });
 };
+
+const validate = async () => {
+  // When the package contents are updated *and validated*, update the expected below by running 'find Assembled | pbcopy' and pasting it over the expected string.
+  // This helps ensure a bad package is not accidentally published due to tooling changes, etc.
+  const expected =
+`Assembled
+Assembled/EngineHook.ts
+Assembled/EngineView.tsx
+Assembled/ios
+Assembled/ios/BabylonNativeInterop.mm
+Assembled/ios/libs
+Assembled/ios/libs/libxr.a
+Assembled/ios/libs/libMachineIndependent.a
+Assembled/ios/libs/libWindow.a
+Assembled/ios/libs/libbimg.a
+Assembled/ios/libs/libOGLCompiler.a
+Assembled/ios/libs/libastc.a
+Assembled/ios/libs/libNativeWindow.a
+Assembled/ios/libs/libNativeEngine.a
+Assembled/ios/libs/libNativeXr.a
+Assembled/ios/libs/libspirv-cross-glsl.a
+Assembled/ios/libs/libNativeInput.a
+Assembled/ios/libs/libJsRuntime.a
+Assembled/ios/libs/libOSDependent.a
+Assembled/ios/libs/libastc-codec.a
+Assembled/ios/libs/libGenericCodeGen.a
+Assembled/ios/libs/libspirv-cross-core.a
+Assembled/ios/libs/libspirv-cross-msl.a
+Assembled/ios/libs/libspirv-cross-hlsl.a
+Assembled/ios/libs/libbx.a
+Assembled/ios/libs/libnapi.a
+Assembled/ios/libs/libBabylonNative.a
+Assembled/ios/libs/libSPIRV.a
+Assembled/ios/libs/libbgfx.a
+Assembled/ios/libs/libglslang.a
+Assembled/ios/EngineViewManager.mm
+Assembled/ios/BabylonNativeInterop.h
+Assembled/ios/ReactNativeBabylon.xcodeproj
+Assembled/ios/ReactNativeBabylon.xcodeproj/project.pbxproj
+Assembled/ios/ReactNativeBabylon.xcodeproj/project.xcworkspace
+Assembled/ios/ReactNativeBabylon.xcodeproj/project.xcworkspace/xcshareddata
+Assembled/ios/ReactNativeBabylon.xcodeproj/project.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
+Assembled/ios/BabylonModule.mm
+Assembled/ios/BabylonNative.h
+Assembled/README.md
+Assembled/EngineHelpers.ts
+Assembled/package.json
+Assembled/android
+Assembled/android/build.gradle
+Assembled/android/src
+Assembled/android/src/main
+Assembled/android/src/main/AndroidManifest.xml
+Assembled/android/src/main/java
+Assembled/android/src/main/java/com
+Assembled/android/src/main/java/com/reactlibrary
+Assembled/android/src/main/java/com/reactlibrary/BabylonNativeInterop.java
+Assembled/android/src/main/java/com/reactlibrary/BabylonModule.java
+Assembled/android/src/main/java/com/reactlibrary/BabylonPackage.java
+Assembled/android/src/main/java/com/reactlibrary/EngineViewManager.java
+Assembled/android/src/main/java/com/reactlibrary/EngineView.java
+Assembled/android/src/main/jniLibs
+Assembled/android/src/main/jniLibs/armeabi-v7a
+Assembled/android/src/main/jniLibs/armeabi-v7a/libBabylonNative.so
+Assembled/android/src/main/jniLibs/x86
+Assembled/android/src/main/jniLibs/x86/libBabylonNative.so
+Assembled/android/src/main/jniLibs/arm64-v8a
+Assembled/android/src/main/jniLibs/arm64-v8a/libBabylonNative.so
+Assembled/react-native-babylon.podspec
+Assembled/index.ts
+Assembled/BabylonModule.ts
+`;
+
+  const result = shelljs.exec('find Assembled', {silent: true});
+  if (result.stdout != expected) {
+    throw `Expected:\n${expected}\n\nActual:\n${result.stdout}`;
+  }
+}
 
 const createPackage = async () => {
   exec('npm pack', 'Assembled');
@@ -89,7 +166,7 @@ const createPackage = async () => {
 
 const copyFiles = gulp.parallel(copyCommonFiles, copyIOSFiles, copyAndroidFiles);
 
-const build = gulp.series(buildIOS, buildAndroid, createIOSUniversalLibs, copyFiles);
+const build = gulp.series(buildIOS, buildAndroid, createIOSUniversalLibs, copyFiles, validate);
 const rebuild = gulp.series(clean, build);
 const pack = gulp.series(rebuild, createPackage);
 
