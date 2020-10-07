4da8f36ca3e4f7095eee503f40cbab3f1eaf4bab
Gary Hsu <bghgary@users.noreply.github.com>
2020-10-07 12:45:42 -0700

    Fix typo

--- Modules/@babylonjs/react-native/ios/BabylonNative.cpp
+++ Modules/@babylonjs/react-native/ios/BabylonNative.cpp
@@ -60,7 +60,7 @@ namespace Babylon
         JsRuntime::DispatchFunctionT dispatchFunction =
             [env = m_impl->env, data = std::make_shared<DispatchData>(m_env)](std::function<void(Napi::Env)> func)
             {
-                (*run_loop_scheduler)([env, func = std::move(func), &data]()
+                (*data->scheduler)([env, func = std::move(func), &data]()
                 {
                     func(env);
                     data->flushedQueue.Call({});
