ba05041b3eb8276563b52ed5d3c5de5cf369819f
Chris Barth <chriba@microsoft.com>
2021-01-06 11:03:35 -0800

    remove debug logic

--- Apps/Playground/index.js
+++ Apps/Playground/index.js
@@ -2,13 +2,8 @@
  * @format
  */
 
-debugger;
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-try {
-    AppRegistry.registerComponent(appName, () => App);
-} catch (error) {
-    console.log(error);
-}
+AppRegistry.registerComponent(appName, () => App);
