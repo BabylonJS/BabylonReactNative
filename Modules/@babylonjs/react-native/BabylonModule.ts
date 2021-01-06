6055788d1da8a3ac15e11ef40bdf4c7f289f554c
Chris Barth <chriba@microsoft.com>
2021-01-06 12:23:31 -0800

    revert files

--- Modules/@babylonjs/react-native/BabylonModule.ts
+++ Modules/@babylonjs/react-native/BabylonModule.ts
@@ -2,62 +2,30 @@ import { NativeModules } from 'react-native';
 import { NativeEngine } from '@babylonjs/core';
 
 // This global object is part of Babylon Native.
-console.log("Declaring _native");
 declare const _native: {
     graphicsInitializationPromise: Promise<void>;
     engineInstance: NativeEngine;
 }
-try {
-    console.log("_native:" + _native);
-} catch (error) {
-    console.log(error.stack);
-}
 
-console.log("Declaring NativeBabylonModule");
 const NativeBabylonModule: {
     initialize(): Promise<boolean>;
     whenInitialized(): Promise<boolean>;
 } = NativeModules.BabylonModule;
-try {
-    console.log("NativeBabylonModule:" + NativeBabylonModule);
-} catch (error) {
-    console.log(error.stack);
-}
 
-console.log("Declaring BabylonModule");
 export const BabylonModule = {
     initialize: async () => {
-        try {
-            console.log("Calling NativeBabylonModule.initialize");
-            const initialized = await NativeBabylonModule.initialize();
-            if (initialized) {
-                console.log("Awaiting graphics initialization promise");
-                await _native.graphicsInitializationPromise;
-            }
-
-            return initialized;
-        } catch (error) {
-            console.log(error.stack);
-            throw error;
+        const initialized = await NativeBabylonModule.initialize();
+        if (initialized) {
+            await _native.graphicsInitializationPromise;
         }
+        return initialized;
     },
 
     whenInitialized: NativeBabylonModule.whenInitialized,
 
     createEngine: () => {
-        try {
-            console.log("Creating NativeEngine");
-            const engine = new NativeEngine();
-            _native.engineInstance = engine;
-            return engine;
-        } catch (error) {
-            console.log(error.stack);
-            throw error;
-        }
+        const engine = new NativeEngine();
+        _native.engineInstance = engine;
+        return engine;
     }
 };
-try {
-    console.log("BabylonModule:" + BabylonModule);
-} catch (error) {
-    console.log(error.stack);
-}
--- Modules/@babylonjs/react-native/EngineHelpers.ts
+++ Modules/@babylonjs/react-native/EngineHelpers.ts
@@ -2,28 +2,13 @@ import { Engine } from '@babylonjs/core';
 
 const disposedPropertyName = "EngineHelper_IsDisposed";
 
-console.log("Declaring IsEngineDisposed");
 export function IsEngineDisposed(engine: Engine): boolean {
-    try {
-        console.log("IsEngineDisposed called");
-        return (engine as any)[disposedPropertyName];
-    } catch (error) {
-        console.log(error.stack);
-        throw error;
-    }
+    return (engine as any)[disposedPropertyName];
 }
-console.log("IsEngineDisposed:" + IsEngineDisposed);
 
-console.log("Declaring DisposeEngine");
 export function DisposeEngine(engine: Engine) {
-    try {
-        console.log("DisposeEngine called");
-        if (engine && !IsEngineDisposed(engine)) {
-            engine.dispose();
-            (engine as any)[disposedPropertyName] = true;
-        }
-    } catch (error) {
-        console.log(error.stack);
+    if (engine && !IsEngineDisposed(engine)) {
+        engine.dispose();
+        (engine as any)[disposedPropertyName] = true;
     }
 }
-console.log("DisposeEngine:" + DisposeEngine);
