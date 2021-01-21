f7711af25381ac29ae86562482ba2904c3903da6
Chris Barth <chriba@microsoft.com>
2021-01-21 13:30:13 -0800

    remove additional solution to prevent react-native-windows version conflicts

--- Modules/@babylonjs/react-native/windows/scripts/Build.ps1
+++ Modules/@babylonjs/react-native/windows/scripts/Build.ps1
@@ -1,10 +1,10 @@
 Import-Module $PSScriptRoot\Utils.psm1
 
-Compile-Solution -Platform "x86" -Configuration "Debug" -Solution "$PSScriptRoot\..\BabylonReactNative.sln"
-Compile-Solution -Platform "x86" -Configuration "Release" -Solution "$PSScriptRoot\..\BabylonReactNative.sln"
-Compile-Solution -Platform "x64" -Configuration "Release" -Solution "$PSScriptRoot\..\BabylonReactNative.sln"
-Compile-Solution -Platform "x64" -Configuration "Debug" -Solution "$PSScriptRoot\..\BabylonReactNative.sln"
-Compile-Solution -Platform "arm" -Configuration "Debug" -Solution "$PSScriptRoot\..\BabylonReactNative.sln"
-Compile-Solution -Platform "arm" -Configuration "Release" -Solution "$PSScriptRoot\..\BabylonReactNative.sln"
-Compile-Solution -Platform "ARM64" -Configuration "Release" -Solution "$PSScriptRoot\..\BabylonReactNative.sln"
-Compile-Solution -Platform "ARM64" -Configuration "Debug" -Solution "$PSScriptRoot\..\BabylonReactNative.sln"
+Compile-Solution -Platform "Win32" -Configuration "Debug" -Solution "$PSScriptRoot\..\..\submodules\BabylonNative\Build_uwp_x86\ReactNativeBabylon.sln"
+Compile-Solution -Platform "Win32" -Configuration "Release" -Solution "$PSScriptRoot\..\..\submodules\BabylonNative\Build_uwp_x86\ReactNativeBabylon.sln"
+Compile-Solution -Platform "x64" -Configuration "Release" -Solution "$PSScriptRoot\..\..\submodules\BabylonNative\Build_uwp_x64\ReactNativeBabylon.sln"
+Compile-Solution -Platform "x64" -Configuration "Debug" -Solution "$PSScriptRoot\..\..\submodules\BabylonNative\Build_uwp_x64\ReactNativeBabylon.sln"
+Compile-Solution -Platform "arm" -Configuration "Debug" -Solution "$PSScriptRoot\..\..\submodules\BabylonNative\Build_uwp_arm\ReactNativeBabylon.sln"
+Compile-Solution -Platform "arm" -Configuration "Release" -Solution "$PSScriptRoot\..\..\submodules\BabylonNative\Build_uwp_arm\ReactNativeBabylon.sln"
+Compile-Solution -Platform "ARM64" -Configuration "Release" -Solution "$PSScriptRoot\..\..\submodules\BabylonNative\Build_uwp_arm64\ReactNativeBabylon.sln"
+Compile-Solution -Platform "ARM64" -Configuration "Debug" -Solution "$PSScriptRoot\..\..\submodules\BabylonNative\Build_uwp_arm64\ReactNativeBabylon.sln"
