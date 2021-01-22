749dff39eee53c3eb8fa5e926dbcde817f3fdbec
Chris Barth <chriba@microsoft.com>
2021-01-22 15:07:49 -0800

    initialize openxr headers

--- Modules/@babylonjs/react-native/windows/scripts/PRSetup.ps1
+++ Modules/@babylonjs/react-native/windows/scripts/PRSetup.ps1
@@ -10,7 +10,7 @@ git submodule update --init --recursive "Dependencies\base-n"
 git submodule update --init --recursive "Dependencies\bgfx.cmake"
 git submodule update --init --recursive "Dependencies\glslang"
 git submodule update --init --recursive "Dependencies\xr\Dependencies\OpenXR-SDK"
-git submodule update --init --recursive "Dependencies\xr\Dependencies\OpenXR-SDK"
+git submodule update --init --recursive "Dependencies\xr\Dependencies\OpenXR-MixedReality"
 Pop-Location
 
 Restore-CMakeProject -Platform "x64"
