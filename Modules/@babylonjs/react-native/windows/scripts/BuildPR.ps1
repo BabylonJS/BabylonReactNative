dff58b534e4b1e94fe44f0ae64a2cba8ec90f030
Chris Barth <chriba@microsoft.com>
2021-01-22 13:07:32 -0800

    try adding some pr validation

---
+++ Modules/@babylonjs/react-native/windows/scripts/BuildPR.ps1
@@ -0,0 +1,4 @@
+Import-Module $PSScriptRoot\Utils.psm1
+
+Compile-Solution -Platform "x64" -Configuration "Debug" -Solution "$PSScriptRoot\..\..\submodules\BabylonNative\Build_uwp_x64\ReactNativeBabylon.sln"
+Compile-Solution -Platform "x64" -Configuration "Debug" -Solution "$PSScriptRoot\..\..\..\..\..\Apps\Playground\windows\Playground.sln"
