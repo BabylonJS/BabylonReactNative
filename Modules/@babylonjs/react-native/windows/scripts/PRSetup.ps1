42d4ce77417779dee2988d7b612c5afc35e1c869
Chris Barth <chriba@microsoft.com>
2021-01-22 14:41:13 -0800

    try different setup process with manual git submodule initialization

--- Modules/@babylonjs/react-native/windows/scripts/PRSetup.ps1
+++ Modules/@babylonjs/react-native/windows/scripts/PRSetup.ps1
@@ -1,12 +1,15 @@
 Import-Module $PSScriptRoot\Utils.psm1
 
 git submodule update --init "$PSScriptRoot\..\..\submodules\BabylonNative"
-git submodule update --init --recursive "$PSScriptRoot\..\..\submodules\BabylonNative\Dependencies\SPIRV-Cross"
-git submodule update --init --recursive "$PSScriptRoot\..\..\submodules\BabylonNative\Dependencies\arcana.cpp"
-git submodule update --init --recursive "$PSScriptRoot\..\..\submodules\BabylonNative\Dependencies\base-n"
-git submodule update --init --recursive "$PSScriptRoot\..\..\submodules\BabylonNative\Dependencies\bgfx.cmake"
-git submodule update --init --recursive "$PSScriptRoot\..\..\submodules\BabylonNative\Dependencies\glslang"
-git submodule update --init --recursive "$PSScriptRoot\..\..\submodules\BabylonNative\Dependencies\xr\Dependencies\OpenXR-SDK"
-git submodule update --init --recursive "$PSScriptRoot\..\..\submodules\BabylonNative\Dependencies\xr\Dependencies\OpenXR-SDK"
+
+Push-Location "$PSScriptRoot\..\..\submodules\BabylonNative"
+git submodule update --init --recursive "Dependencies\SPIRV-Cross"
+git submodule update --init --recursive "Dependencies\arcana.cpp"
+git submodule update --init --recursive "Dependencies\base-n"
+git submodule update --init --recursive "Dependencies\bgfx.cmake"
+git submodule update --init --recursive "Dependencies\glslang"
+git submodule update --init --recursive "Dependencies\xr\Dependencies\OpenXR-SDK"
+git submodule update --init --recursive "Dependencies\xr\Dependencies\OpenXR-SDK"
+Pop-Location
 
 Restore-CMakeProject -Platform "x64"
