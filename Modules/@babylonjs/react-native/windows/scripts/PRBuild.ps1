b35bcb198d0194dd3984c77933981896acab02cb
Chris Barth <chriba@microsoft.com>
2021-01-22 15:21:24 -0800

    add nuget restore to pr validation

--- Modules/@babylonjs/react-native/windows/scripts/PRBuild.ps1
+++ Modules/@babylonjs/react-native/windows/scripts/PRBuild.ps1
@@ -1,6 +1,5 @@
 Import-Module $PSScriptRoot\Utils.psm1
 
-nuget restore "$PSScriptRoot\..\..\submodules\BabylonNative\Build_uwp_x64\ReactNativeBabylon.sln"
 Compile-Solution -Platform "x64" -Configuration "Debug" -Solution "$PSScriptRoot\..\..\submodules\BabylonNative\Build_uwp_x64\ReactNativeBabylon.sln"
 nuget restore "$PSScriptRoot\..\..\..\..\..\Apps\Playground\windows\Playground.sln"
 Compile-Solution -Platform "x64" -Configuration "Debug" -Solution "$PSScriptRoot\..\..\..\..\..\Apps\Playground\windows\Playground.sln"
