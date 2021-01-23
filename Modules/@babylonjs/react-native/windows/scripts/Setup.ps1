7afc698d31b13138a879ef4abd63628bdb44e8f7
Chris Barth <chriba@microsoft.com>
2021-01-22 16:56:10 -0800

    add git submodule initialization to setup.ps1

--- Modules/@babylonjs/react-native/windows/scripts/Setup.ps1
+++ Modules/@babylonjs/react-native/windows/scripts/Setup.ps1
@@ -1,5 +1,18 @@
 Import-Module $PSScriptRoot\Utils.psm1
 
+# windows build agents don't support the path lengths required for initializing arcore dependencies,
+# so we manually initialize the submodules we need here.
+git submodule update --init "$PSScriptRoot\..\..\submodules\BabylonNative"
+Push-Location "$PSScriptRoot\..\..\submodules\BabylonNative"
+git submodule update --init --recursive "Dependencies\SPIRV-Cross"
+git submodule update --init --recursive "Dependencies\arcana.cpp"
+git submodule update --init --recursive "Dependencies\base-n"
+git submodule update --init --recursive "Dependencies\bgfx.cmake"
+git submodule update --init --recursive "Dependencies\glslang"
+git submodule update --init --recursive "Dependencies\xr\Dependencies\OpenXR-SDK"
+git submodule update --init --recursive "Dependencies\xr\Dependencies\OpenXR-MixedReality"
+Pop-Location
+
 Restore-CMakeProject -Platform "x86"
 Restore-CMakeProject -Platform "x64"
 Restore-CMakeProject -Platform "arm"
