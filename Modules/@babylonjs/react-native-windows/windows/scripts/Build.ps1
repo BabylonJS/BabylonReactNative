69203e70c33a3b887e078959ac987cd241aea77f
C. M. Barth <chriba@microsoft.com>
2021-02-08 16:07:49 -0800

    Move windows content into a @babylonjs/react-native-windows package (#154)
    * move react-native-windows content
    
    * working react-native-windows builds
    
    * rearrange git status
    
    * avoid uploading for new windows package publishing
    
    * change publish directory for windows
    
    * working windows build?
    
    * remove unused packages
    
    * add bundle step to build pr
    
    * remove unneeded bundle step
    
    * test publish
    
    * add back @babylonjs/react-native packaging
    
    * avoid checking in an installed package-lock.json for @babylonjs/react-native
    
    * confirm packaging works
    
    * add peer dependency on @babylonjs/react-native
    
    * update package lock

---
+++ Modules/@babylonjs/react-native-windows/windows/scripts/Build.ps1
@@ -0,0 +1,10 @@
+Import-Module $PSScriptRoot\Utils.psm1
+
+Compile-Solution -Platform "Win32" -Configuration "Debug" -Solution "$PSScriptRoot\..\..\..\react-native\submodules\BabylonNative\Build_uwp_x86\ReactNativeBabylon.sln"
+Compile-Solution -Platform "Win32" -Configuration "Release" -Solution "$PSScriptRoot\..\..\..\react-native\submodules\BabylonNative\Build_uwp_x86\ReactNativeBabylon.sln"
+Compile-Solution -Platform "x64" -Configuration "Release" -Solution "$PSScriptRoot\..\..\..\react-native\submodules\BabylonNative\Build_uwp_x64\ReactNativeBabylon.sln"
+Compile-Solution -Platform "x64" -Configuration "Debug" -Solution "$PSScriptRoot\..\..\..\react-native\submodules\BabylonNative\Build_uwp_x64\ReactNativeBabylon.sln"
+Compile-Solution -Platform "arm" -Configuration "Debug" -Solution "$PSScriptRoot\..\..\..\react-native\submodules\BabylonNative\Build_uwp_arm\ReactNativeBabylon.sln"
+Compile-Solution -Platform "arm" -Configuration "Release" -Solution "$PSScriptRoot\..\..\..\react-native\submodules\BabylonNative\Build_uwp_arm\ReactNativeBabylon.sln"
+Compile-Solution -Platform "ARM64" -Configuration "Release" -Solution "$PSScriptRoot\..\..\..\react-native\submodules\BabylonNative\Build_uwp_arm64\ReactNativeBabylon.sln"
+Compile-Solution -Platform "ARM64" -Configuration "Debug" -Solution "$PSScriptRoot\..\..\..\react-native\submodules\BabylonNative\Build_uwp_arm64\ReactNativeBabylon.sln"
