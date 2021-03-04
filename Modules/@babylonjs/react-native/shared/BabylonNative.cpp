0b967604fe24e1e81ff9218a04e0bcb7c72ded7e
nkari82 <nkari82@gmail.com>
2021-03-04 19:33:13 +0900

    Update Modules/@babylonjs/react-native/shared/BabylonNative.cpp
    Co-authored-by: rgerd <rgerd@users.noreply.github.com>

--- Modules/@babylonjs/react-native/shared/BabylonNative.cpp
+++ Modules/@babylonjs/react-native/shared/BabylonNative.cpp
@@ -144,7 +144,6 @@ namespace Babylon
                 }
             });
         }
-       			
         void SetMouseButtonState(uint32_t buttonId, bool isDown, uint32_t x, uint32_t y)
         {
             if (isDown)
