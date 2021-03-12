779a374c36024d7309dd42b8da6774ef151ac404
Chris Barth <chriba@microsoft.com>
2021-03-12 13:21:51 -0800

    enable fast refresh for UWP scenarios

--- Apps/PackageTest/0.64.0-rc.0/windows/PackageTest/App.cpp
+++ Apps/PackageTest/0.64.0-rc.0/windows/PackageTest/App.cpp
@@ -25,9 +25,10 @@ App::App() noexcept
     InstanceSettings().UseWebDebugger(false);
     InstanceSettings().UseFastRefresh(false);
 #else
-    JavaScriptBundleFile(L"index");
-    InstanceSettings().UseWebDebugger(true);
+    JavaScriptMainModuleName(L"index");
+    InstanceSettings().UseWebDebugger(false); // BabylonReactNative accesses the jsi runtime, which isn't possible with the web debugger
     InstanceSettings().UseFastRefresh(true);
+    InstanceSettings().SourceBundleHost(L"localhost"); // Update to PC ip address when running on remote machines
 #endif
 
 #if _DEBUG
