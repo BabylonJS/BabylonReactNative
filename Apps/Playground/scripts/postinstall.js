cdc8916efcf81659ff78df962972edab0eae27e1
Chris Barth <chriba@microsoft.com>
2021-04-22 12:50:52 -0700

    update script comment

--- Apps/Playground/scripts/postinstall.js
+++ Apps/Playground/scripts/postinstall.js
@@ -14,7 +14,7 @@ else {
     const parts = output.split("npm@");
     const version = parts[1].trim();
 
-    // Check if using non 6.13.* npm version
+    // Check if using non 6.*.* npm version
     if (!version.match(/6\.\d+\.\d+/g)) {
         throw `Error: BabylonReactNative Playground development requires npm version 6.13.*, Your current npm version is ${version}. Run npm install -g npm@6.13 to update your npm version.`;
     }
