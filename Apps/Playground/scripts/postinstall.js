1012a411c3663978fc9d1b5ef03290c4e6074699
Chris Barth <chriba@microsoft.com>
2021-04-22 12:58:46 -0700

    use npm --version instead

--- Apps/Playground/scripts/postinstall.js
+++ Apps/Playground/scripts/postinstall.js
@@ -1,20 +1,6 @@
 const shelljs = require('shelljs');
-let output = shelljs.exec('npm list -g npm');
+const version = shelljs.exec('npm --version');
 
-if (!output.includes("npm@")) {
-    shelljs.echo("global npm install not found");
-    output = shelljs.exec('npm list npm');
-}
-
-if (!output.includes("npm@")) {
-    shelljs.echo("local npm version not found");
-}
-else {
-    const parts = output.split("npm@");
-    const version = parts[1].trim();
-
-    // Check if using non 6.*.* npm version
-    if (!version.match(/6\.\d+\.\d+/g)) {
-        throw `Error: BabylonReactNative Playground development requires npm version 6.13.*, Your current npm version is ${version}. Run npm install -g npm@6.13 to update your npm version.`;
-    }
+if (!version.trim().match(/6\.\d+\.\d+/g)) {
+    throw `Error: BabylonReactNative Playground development requires npm version 6.13.*, Your current npm version is ${version}. Run npm install -g npm@6.13 to update your npm version.`;
 }
