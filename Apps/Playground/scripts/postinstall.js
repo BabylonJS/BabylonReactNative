b2a9abbdc69ad24c18eca2fe07dee05b4aad900a
Chris Barth <chriba@microsoft.com>
2021-04-22 12:30:28 -0700

    avoid failing if no npm version is found

--- Apps/Playground/scripts/postinstall.js
+++ Apps/Playground/scripts/postinstall.js
@@ -1,10 +1,21 @@
 const shelljs = require('shelljs');
-const output = shelljs.exec('npm list -g npm');
-const parts = output.split("npm@");
-const version = parts[1].trim();
+let output = shelljs.exec('npm list -g npm');
 
-// Check if using npm version 7+
-if (version.charAt(0) == '7')
-{
-  throw `Error: BabylonReactNative Playground development requires npm version 6.13.*, Your current npm version is ${version}. Run npm install -g npm@6.13 to update your npm version.`;
+if (!output.includes("npm@")) {
+    shelljs.echo("global npm install not found");
+    output = shelljs.exec('npm list npm');
+}
+
+if (!output.includes("npm@")) {
+    shelljs.echo("local npm version not found");
+
+}
+else {
+    const parts = output.split("npm@");
+    const version = parts[1].trim();
+
+    // Check if using npm version 7+
+    if (version.charAt(0) == '7') {
+        throw `Error: BabylonReactNative Playground development requires npm version 6.13.*, Your current npm version is ${version}. Run npm install -g npm@6.13 to update your npm version.`;
+    }
 }
