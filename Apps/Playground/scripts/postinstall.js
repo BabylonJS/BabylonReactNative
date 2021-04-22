f1058a04299d547f24eb5c810af9b356b9e25abd
Chris Barth <chriba@microsoft.com>
2021-04-22 12:35:07 -0700

    address review comments

--- Apps/Playground/scripts/postinstall.js
+++ Apps/Playground/scripts/postinstall.js
@@ -14,8 +14,8 @@ else {
     const parts = output.split("npm@");
     const version = parts[1].trim();
 
-    // Check if using npm version 7+
-    if (version.charAt(0) == '7') {
+    // Check if using non 6.13.* npm version
+    if (!version.match(/6\.13\..*/g)) {
         throw `Error: BabylonReactNative Playground development requires npm version 6.13.*, Your current npm version is ${version}. Run npm install -g npm@6.13 to update your npm version.`;
     }
 }
