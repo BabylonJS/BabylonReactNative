d3aef65d9071d2fb8c2ece4c13b2625bb9d44146
Ryan Tremblay <ryan.tremblay@microsoft.com>
2021-12-02 18:12:23 -0800

    Upgrade to NPM 8.1 (#316)
    * Upgrade to NPM 8.1

--- Apps/Playground/scripts/tools.js
+++ Apps/Playground/scripts/tools.js
@@ -10,10 +10,6 @@ function iosCmake() {
 function postInstall() {
   const version = shelljs.exec('npm --version', {silent: true});
 
-  if (!version.trim().match(/6\.\d+\.\d+/g)) {
-    throw `Error: BabylonReactNative Playground development requires npm version 6.13.*, Your current npm version is ${version}. Run npm install -g npm@6.13 to update your npm version.`;
-  }
-
   console.log(chalk.black.bgCyan('Installing Babylon React Native npm packages...'));
   shelljs.exec('npm install', {cwd: '../../Modules/@babylonjs/react-native'});
 
