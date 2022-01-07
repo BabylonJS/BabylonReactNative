1b088ebecb3bad6a2b09acdce2d006ea88b468f7
Cedric Guillemet <ceguille@microsoft.com>
2022-01-07 11:32:54 +0100

    playground shared

--- Apps/Playground/index.js
+++ Apps/Playground/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import App from '@babylonjs/playground-shared/App';
+import {name as appName} from '@babylonjs/playground-shared/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
