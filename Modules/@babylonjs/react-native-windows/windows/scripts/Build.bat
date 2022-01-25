5e2ad3c0b2d80f9f866d2da09b3befc5384354e0
Gary Hsu <bghgary@users.noreply.github.com>
2022-01-24 17:43:34 -0800

    Fix issues when entering XR (#328)
    - Update Babylon.js npm packages to 5.0.0-alpha.65
    * Update Babylon Native submodule to latest
    * Set max-old-space-size=8192 for Windows build

--- Modules/@babylonjs/react-native-windows/windows/scripts/Build.bat
+++ Modules/@babylonjs/react-native-windows/windows/scripts/Build.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 SETLOCAL
+SET NODE_OPTIONS="--max-old-space-size=8192"
 SET PowerShellScriptPath=%~dpn0.ps1
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' %1 %2 %3 %4;exit $LASTEXITCODE"
