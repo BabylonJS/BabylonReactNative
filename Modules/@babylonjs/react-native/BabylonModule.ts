409f23278ed9f5f0c062a66453e89512e37f6a39
Lucas <lgxm3z@gmail.com>
2022-07-22 18:19:38 -0300

    fix ensureInitialized

--- Modules/@babylonjs/react-native/BabylonModule.ts
+++ Modules/@babylonjs/react-native/BabylonModule.ts
@@ -18,13 +18,9 @@ export async function ensureInitialized(): Promise<boolean> {
         // JSI (which Babylon Native uses heavily) can not work in this mode. In the future, this debugging mode will be phased out as it is incompatible with TurboModules for the same reason.
         return false;
     } else {
-        if(BabylonModule){ 
-            // This does the first stage of Babylon Native initialization, including creating the BabylonNative JSI object.
-            await BabylonModule.initialize();
-            return true;
-        } else{
-            return false;
-        }
+        // This does the first stage of Babylon Native initialization, including creating the BabylonNative JSI object.
+        if(BabylonModule) await BabylonModule.initialize();
+        return true;
     }
 }
 
