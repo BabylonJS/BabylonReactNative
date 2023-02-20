4b47366608fee6e8ec4ba919ddfafc218159b03f
Cedric Guillemet <ceguille@microsoft.com>
2023-02-20 15:03:30 +0100

    another test at options for node18

--- Modules/@babylonjs/react-native-windows/windows/scripts/PRBuild.bat
+++ Modules/@babylonjs/react-native-windows/windows/scripts/PRBuild.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SETLOCAL
-SET NODE_OPTIONS="--max-old-space-size=8192 --openssl-legacy-provider"
+SET NODE_OPTIONS="--max-old-space-size=8192 --openssl-legacy-provider umi build"
 SET PowerShellScriptPath=%~dpn0.ps1
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' %1 %2;exit $LASTEXITCODE"
