f342bae527044ed434a94c80b896d27717697b30
Cedric Guillemet <cedric.guillemet@gmail.com>
2023-09-20 17:01:02 +0200

    fix permission for TS package

--- Package/gulpfile.js
+++ Package/gulpfile.js
@@ -654,6 +654,7 @@ const patchPackageVersion = async () => {
           packageJsonWindows["name"] = "@babylonjs/react-native-basekit-windows" + packageNamePostfix;
           delete packageJsoniOSAndroid['peerDependencies']['react-native-permissions'];
           delete packageJsonWindows['peerDependencies']['react-native-permissions'];
+          delete packageJson['peerDependencies']['react-native-permissions'];
         } else {
           packageJsoniOSAndroid["name"] = "@babylonjs/react-native-iosandroid" + packageNamePostfix;
           packageJsonWindows["name"] = "@babylonjs/react-native-windows" + packageNamePostfix;
