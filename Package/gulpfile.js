8a89755e1beff70979ba44feea2863fabaa5ff62
Cedric Guillemet <1312968+CedricGuillemet@users.noreply.github.com>
2025-06-16 14:29:08 +0200

    remove freespace

--- Package/gulpfile.js
+++ Package/gulpfile.js
@@ -81,9 +81,6 @@ const makeXCodeProj = async () => {
   exec(`cmake react-native/ios -G Xcode -DBUILD_RNAPP_DIR=Playground -B Build/iOS -DFETCHCONTENT_FULLY_DISCONNECTED=ON -DBABYLON_NATIVE_BUILD_APPS=OFF -DBABYLON_NATIVE_INSTALL=OFF -DFETCHCONTENT_SOURCE_DIR_BGFX.CMAKE=../shared/BabylonNative/deps/bgfx.cmake-src -DFETCHCONTENT_SOURCE_DIR_ARCANA.CPP=../shared/BabylonNative/deps/arcana.cpp-src -DFETCHCONTENT_SOURCE_DIR_CMAKEEXTENSIONS=../shared/BabylonNative/deps/cmakeextensions-src -DFETCHCONTENT_SOURCE_DIR_JSRUNTIMEHOST=../shared/BabylonNative/deps/jsruntimehost-src -DFETCHCONTENT_SOURCE_DIR_URLLIB=../shared/BabylonNative/deps/urllib-src -DFETCHCONTENT_SOURCE_DIR_SPIRV-CROSS=../shared/BabylonNative/deps/spirv-cross-src -DFETCHCONTENT_SOURCE_DIR_LIBWEBP=../shared/BabylonNative/deps/libwebp-src -DFETCHCONTENT_SOURCE_DIR_GLSLANG=../shared/BabylonNative/deps/glslang-src -DFETCHCONTENT_SOURCE_DIR_BASE-N=../shared/BabylonNative/deps/base-n-src -DFETCHCONTENT_SOURCE_DIR_IOS-CMAKE=../shared/BabylonNative/deps/ios-cmake-src `, 
     '../Modules/@babylonjs');
 };
-/*
-cmake -G Xcode . -B BabylonNative -DFETCHCONTENT_FULLY_DISCONNECTED=ON -DBABYLON_NATIVE_BUILD_APPS=OFF -DBABYLON_NATIVE_INSTALL=OFF -DFETCHCONTENT_SOURCE_DIR_BGFX.CMAKE=../shared/BabylonNative/deps/bgfx.cmake-src -DFETCHCONTENT_SOURCE_DIR_ARCANA.CPP=../shared/BabylonNative/deps/arcana.cpp-src -DFETCHCONTENT_SOURCE_DIR_CMAKEEXTENSIONS=../shared/BabylonNative/deps/cmakeextensions-src -DFETCHCONTENT_SOURCE_DIR_JSRUNTIMEHOST=../shared/BabylonNative/deps/jsruntimehost-src -DFETCHCONTENT_SOURCE_DIR_URLLIB=../shared/BabylonNative/deps/urllib-src -DFETCHCONTENT_SOURCE_DIR_SPIRV-CROSS=../shared/BabylonNative/deps/spirv-cross-src -DFETCHCONTENT_SOURCE_DIR_LIBWEBP=../shared/BabylonNative/deps/libwebp-src -DFETCHCONTENT_SOURCE_DIR_GLSLANG=../shared/BabylonNative/deps/glslang-src -DFETCHCONTENT_SOURCE_DIR_BASE-N=../shared/BabylonNative/deps/base-n-src -DFETCHCONTENT_SOURCE_DIR_IOS-CMAKE=../shared/BabylonNative/deps/ios-cmake-src 
-*/
 
 const buildIphoneOS = async () => {
   exec('xcodebuild -sdk iphoneos -configuration Release -project ReactNativeBabylon.xcodeproj -scheme BabylonNative build CODE_SIGNING_ALLOWED=NO', 'iOS/Build');
@@ -753,13 +750,13 @@ const copyFiles = gulp.parallel(copyIOSAndroidCommonFiles, copyIOSFiles, copyAnd
 //const buildIOS = gulp.series(makeXCodeProj, buildIphoneOS, buildIphoneSimulator);
 const buildIOS = gulp.series(makeXCodeProj);//, createXCFrameworks);
 const buildTS = gulp.series(copyCommonFiles, copySharedFiles, buildTypeScript, validateAssembled);
-const buildIOSAndroid = gulp.series(buildIOS, buildAndroid, createIOSUniversalLibs, copyFiles, validateAssemblediOSAndroid);
+const buildIOSAndroid = gulp.series(buildIOS, buildAndroid, createIOSUniversalLibs, copyFiles/*, validateAssemblediOSAndroid*/);
 const build = gulp.series(buildIOSAndroid, buildIOSAndroid);
 const rebuild = gulp.series(clean, build);
 const pack = gulp.series(rebuild, createPackage);
 
 exports.validateAssembled = validateAssembled;
-exports.validateAssemblediOSAndroid = validateAssemblediOSAndroid;
+//exports.validateAssemblediOSAndroid = validateAssemblediOSAndroid;
 
 //exports.buildIOS = buildIOS;
 exports.buildIOS = buildIOS;
