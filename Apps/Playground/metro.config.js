73570f6b5dfcef551959c0fe66bcd8e615cf69d8
Cedric Guillemet <cedric.guillemet@gmail.com>
2025-06-18 19:27:54 +0200

    metro config

--- Apps/Playground/metro.config.js
+++ Apps/Playground/metro.config.js
@@ -10,6 +10,14 @@ module.exports = makeMetroConfig({
       },
     }),
   },
+  resolver: {
+    extraNodeModules: {
+      '@babylonjs/core': path.resolve(__dirname, './node_modules/@babylonjs/core'),
+      'base-64': path.resolve(__dirname, './node_modules/base-64'),
+      'semver': path.resolve(__dirname, './node_modules/semver'),
+      'react-native-permissions':  path.resolve(__dirname, './node_modules/semver/react-native-permissions')
+    },
+  },
   watchFolders: [
     path.dirname(require.resolve("@babylonjs/react-native/package.json")),
   ],
