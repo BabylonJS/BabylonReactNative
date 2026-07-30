658242d2622a97dea83c499bd0e759fefd70d84f
Cedric Guillemet <1312968+CedricGuillemet@users.noreply.github.com>
2026-07-30 16:14:12 +0200

    up BN

--- Package/gulpfile.js
+++ Package/gulpfile.js
@@ -230,7 +230,7 @@ const createPackage = async () => {
   exec('npm pack', 'Assembled');
 };
 
-const COMMIT_ID = 'b9aab7a366a0b0e81e65945449a3dd9960609d78';
+const COMMIT_ID = 'a4031689b3c4ec08062f0e044b67970eb2382c9f';
 const ZIP_URL = `https://github.com/BabylonJS/BabylonNative/archive/${COMMIT_ID}.zip`;
 const TARGET_DIR = path.resolve(__dirname, '../Modules/@babylonjs/react-native/shared/BabylonNative');
 const ZIP_PATH = path.join(TARGET_DIR, `${COMMIT_ID}.zip`);
